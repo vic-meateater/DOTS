45716313b6f2318a0cce639c4e52b3233a5ff3a6
vic-work <victor.myasoedov@gmail.com>
2023-06-01 16:51:06 +0500

    test 1

---
+++ Assets/Scripts/Speed.cs
@@ -0,0 +1,9 @@
+using Unity.Entities;
+
+namespace zombies
+{
+    public struct Speed : IComponentData
+    {
+        public float value;
+    }
+}
---
+++ Assets/Scripts/SpeedAuthoring.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace zombies
+{
+    public class SpeedAuthoring : MonoBehaviour
+    {
+        public float value;
+    }
+}
